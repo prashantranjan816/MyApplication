fdfcab3a97c6897f23db93a26793b46b69a80e7c
prashantranjan <prashantranjan816@gmail.com>
2019-02-27 01:28:54 +0530

    Initial commit

---
+++ java/com/example/sandeshkini/myapplication/MusicSystem.java
@@ -0,0 +1,13 @@
+package com.example.sandeshkini.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicSystem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_music_system );
+    }
+}
---
+++ java/com/example/sandeshkini/myapplication/PageAdapter.java
@@ -0,0 +1,4 @@
+package com.example.sandeshkini.myapplication;
+
+public class PageAdapter {
+}
